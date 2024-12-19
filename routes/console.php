935a55d61b31543c0ccb5def2b3a9a63726dcae7
ArajaID <arajaid@gmail.com>
2024-12-19 13:23:14 +0700

    ubah tanggal isolir pelanggan

--- routes/console.php
+++ routes/console.php
@@ -16,7 +16,7 @@ Schedule::job(new GenerateTagihan)
 ->monthlyOn(15, '00:01');
 
 Schedule::job(new IsolirPelangganJob)
-->monthlyOn(20, '23:59');
+->monthlyOn(21, '23:59');
 
 Schedule::job(new CekBerlakuPromoJob)->daily();
 
