b24675cb236ec3a0a60bb6bd7d786fd6de0fe9dd
ArajaID <arajaid@gmail.com>
2024-12-22 06:47:06 +0700

    update tagihan controller biaya admin QRIS

--- app/Http/Controllers/TagihanController.php
+++ app/Http/Controllers/TagihanController.php
@@ -10,6 +10,7 @@ use App\Models\Tagihan;
 use App\Models\Pelanggan;
 use App\Models\Transaksi;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TagihanController extends Controller
@@ -31,90 +32,118 @@ class TagihanController extends Controller
     }
 
     public function store(Request $request) {
-        // perintah update tagihan lunas
-        $dataTagihan = Tagihan::where('kode_tagihan', $request->kode_tagihan)->first();
-        // cek apakah pelanggan sedang menggunakan promo
-        $pelanggan = Pelanggan::findOrFail($dataTagihan->id_pelanggan);
-        // cek paket promo yang sedang dipakai
-        $paket = Paket::findOrFail($pelanggan->promo[0]->tambahan_speed);
+        DB::transaction(function() use ($request) {
+            // perintah update tagihan lunas
+            $dataTagihan = Tagihan::where('kode_tagihan', $request->kode_tagihan)->first();
+            // cek apakah pelanggan sedang menggunakan promo
+            $pelanggan = Pelanggan::findOrFail($dataTagihan->id_pelanggan);
+            // cek paket promo yang sedang dipakai
+            $paket = Paket::where('id', $pelanggan->promo[0]->tambahan_speed)->first();
 
-        $bulanTagihan = 'Tagihan ' . date('M') . '-' . date('Y');
+            $bulanTagihan = 'Tagihan ' . date('M') . '-' . date('Y');
 
-        $dataTagihan->update([
-            'status_pembayaran' => 'LUNAS'
-        ]);
+            $dataTagihan->update([
+                'status_pembayaran' => 'LUNAS'
+            ]);
 
-        $request->validate([
-            'jumlah'    => 'required',
-        ]);
+            $request->validate([
+                'jumlah'    => 'required',
+            ]);
 
-        // perintah update comment lunas di secret mikrotik
-        if($dataTagihan->deskripsi == $bulanTagihan) {
-            $client = new Client(config('mikrotik.credential'));
+            // perintah update comment lunas di secret mikrotik
+            if($dataTagihan->deskripsi == $bulanTagihan) {
+                $client = new Client(config('mikrotik.credential'));
 
-            $query = new Query('/ppp/secret/print');
-            $query->where('name', $dataTagihan->pelanggan->user_pppoe);
-            $secrets = $client->query($query)->read();
+                $query = new Query('/ppp/secret/print');
+                $query->where('name', $dataTagihan->pelanggan->user_pppoe);
+                $secrets = $client->query($query)->read();
 
-            foreach ($secrets as $secret) {
-                // perintah remove connection active
-                $queryActive = (new Query('/ppp/active/getall'))
-                            ->where('name', $secret['name']);
-                $response = $client->query($queryActive)->read();
+                foreach ($secrets as $secret) {
+                    // perintah remove connection active
+                    $queryActive = (new Query('/ppp/active/getall'))
+                                ->where('name', $secret['name']);
+                    $response = $client->query($queryActive)->read();
 
-                $queryRemoveActive = (new Query('/ppp/active/remove'))
-                         ->equal('.id', $response[0]['.id']);
-                $client->query($queryRemoveActive)->read();
-                
-                 // kondisi jika pelanggan terisolir
-                 if($secret['profile'] == 'pelanggan-terisolir') {
+                    $queryRemoveActive = (new Query('/ppp/active/remove'))
+                            ->equal('.id', $response[0]['.id']);
+                    $client->query($queryRemoveActive)->read();
                     
-                    // cek pelanggan menggunakan promo atau tidak
-                    if($pelanggan->promo[0]->tambahan_speed) {
-                        $dataPaket = $paket->bandwidth;
-                    } else {
-                        $dataPaket = $dataTagihan->pelanggan->paket->bandwidth;
-                    }
+                    // kondisi jika pelanggan terisolir
+                    if($secret['profile'] == 'pelanggan-terisolir') {
+                        
+                        // cek pelanggan menggunakan promo atau tidak
+                        if($pelanggan->promo[0]->tambahan_speed) {
+                            $dataPaket = $paket->bandwidth;
+                        } else {
+                            $dataPaket = $dataTagihan->pelanggan->paket->bandwidth;
+                        }
 
-                    $query = (new Query('/ppp/secret/set'))
-                    ->equal('.id', $secret['.id'])
-                    ->equal('profile', $dataPaket)
-                    ->equal('comment', 'LUNAS');
-            
-                    $client->query($query)->read();
-                } else {
-                    $query = (new Query('/ppp/secret/set'))
-                    ->equal('.id', $secret['.id'])
-                    ->equal('comment', 'LUNAS');
-            
-                    $client->query($query)->read();
+                        $query = (new Query('/ppp/secret/set'))
+                        ->equal('.id', $secret['.id'])
+                        ->equal('profile', $dataPaket)
+                        ->equal('comment', 'LUNAS');
+                
+                        $client->query($query)->read();
+                    } else {
+                        $query = (new Query('/ppp/secret/set'))
+                        ->equal('.id', $secret['.id'])
+                        ->equal('comment', 'LUNAS');
+                
+                        $client->query($query)->read();
+                    }
                 }
             }
-        }
-        
-        $deskripsiTransaksi = $request->kode_pelanggan . ' - ' . $request->nama_pelanggan;
+            
+            $deskripsiTransaksi = $request->kode_pelanggan . ' - ' . $request->nama_pelanggan;
+            $metodePembayaran = $request->metode_pembayaran;
+            $jumlahDebit = $request->jumlah;
 
-        Transaksi::create([
-            'jenis_transaksi'   => 'Pemasukan',
-            'tanggal'           => $request->tanggal,
-            'sumber'            => $request->kode_tagihan,
-            'debit'             => $request->jumlah,
-            'kategori'          => $request->deskripsi,
-            'metode_pembayaran' => $request->metode_pembayaran,
-            'deskripsi'         => $deskripsiTransaksi
-        ]);
+            // perhitungan uang masuk ke tb transaksi
+            Transaksi::create([
+                'jenis_transaksi'   => 'Pemasukan',
+                'tanggal'           => $request->tanggal,
+                'sumber'            => $request->kode_tagihan,
+                'debit'             => $jumlahDebit,
+                'kategori'          => $request->deskripsi,
+                'metode_pembayaran' => $metodePembayaran,
+                'deskripsi'         => $deskripsiTransaksi
+            ]);
 
-        $debit = 'Rp. ' . number_format($request->jumlah, 0, ',', '.');
-        $tanggalTransaksi = Carbon::parse($request->tanggal)->format('d-m-Y');
-        $tanggalBuat = Carbon::parse($request->created_at)->format('d-m-Y H:i:s');
+            // perhitungan biaya admin penerimaan QRIS
+            if($metodePembayaran == "QRIS") {
+                if($jumlahDebit < 100000) {
+                    $biayaAdmin = $jumlahDebit;
+                } else {
+                    $potongan = 0.3;
+                    $biayaAdmin = ($potongan/100) * $jumlahDebit;
+                }
+            } else {
+                $biayaAdmin = $jumlahDebit;
+            }
 
-        $saldo = 0;
-        $transaksiAll = Transaksi::all();
-        foreach($transaksiAll as $data) {
-            $saldo += $data->debit - $data->kredit;
-        }
-        $formatSaldo = 'Rp. ' . number_format($saldo, 0, ',', '.');
-        
+            $deskripsiBiayaAdmin = 'Admin Bank Penerimaan ' . $request->deskripsi . ' ' . $deskripsiTransaksi ;
+            Transaksi::create([
+                'jenis_transaksi'   => 'Pengeluaran',
+                'tanggal'           => $request->tanggal,
+                'sumber'            => $request->kode_tagihan,
+                'kredit'            => $biayaAdmin,
+                'kategori'          => 'Admin Bank',
+                'metode_pembayaran' => $metodePembayaran,
+                'deskripsi'         => $deskripsiBiayaAdmin
+            ]);
+
+            $debit = 'Rp. ' . number_format($request->jumlah, 0, ',', '.');
+            $tanggalTransaksi = Carbon::parse($request->tanggal)->format('d-m-Y');
+            $tanggalBuat = Carbon::parse($request->created_at)->format('d-m-Y H:i:s');
+
+            $saldo = 0;
+            $transaksiAll = Transaksi::all();
+            foreach($transaksiAll as $data) {
+                $saldo += $data->debit - $data->kredit;
+            }
+            $formatSaldo = 'Rp. ' . number_format($saldo, 0, ',', '.');
+            $biayaAdminFormat = 'Rp. ' . number_format($biayaAdmin, 0, ',', '.');
+            
 
 $htmlText = "*TRANSAKSI UANG MASUK* 🔽";
 $htmlText .= "
@@ -137,7 +166,33 @@ $htmlText .= "
                             'text' => $htmlText,
                             'parse_mode' => 'Markdown'
                         ]);
-        
+            
+
+$htmlTextKeluar = "*TRANSAKSI UANG KELUAR* 🔼";
+$htmlTextKeluar .= "
+✅ *BERHASIL*";
+$htmlTextKeluar .= "
+`===========================`
+`Tanggal   :` $tanggalTransaksi
+`Kategori  :` Admin Bank
+`Deskripsi :` $deskripsiBiayaAdmin
+
+`Jumlah    :` *$biayaAdminFormat*
+`===========================`
+`Saldo     :` *$formatSaldo*
+`===========================`
+`Dibuat    :` $tanggalBuat
+`===========================`
+";
+                        Telegram::sendMessage([
+                            'chat_id' => env('TELEGRAM_CHAT_ID'),
+                            'text' => $htmlTextKeluar,
+                            'parse_mode' => 'Markdown'
+                        ]);
+                    
+                    });
+
+
         toast('Tagihan no ' . $request->kode_tagihan . ' Lunas!','success');
         return redirect()->route('tagihan.index');
     }
