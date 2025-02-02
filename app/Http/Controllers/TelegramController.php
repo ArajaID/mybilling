0e7970f377d0fad3e28b7eeb2b7b9020d712d10f
ArajaID <arajaid@gmail.com>
2025-02-02 19:59:35 +0700

    update telegram tagihan fix number format

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -198,10 +198,10 @@ class TelegramController extends Controller
         if ($tagihan->count() > 0) {
             $message = "*List Tagihan Aktif 😊*\n\n";
             foreach ($tagihan as $key => $value) {
-                $message .= ($key + 1) . ". [" . $value->pelanggan->kode_pelanggan . '] ' . $value->pelanggan->nama_pelanggan . " - " . $value->deskripsi . " - Rp. " . number_format($value->jumlah_tagihan, 0, ',', '.') . "\n\n";
+                $message .= ($key + 1) . ". [" . $value->pelanggan->kode_pelanggan . '] ' . $value->pelanggan->nama_pelanggan . " - " . $value->deskripsi . " - Rp. " . number_format($value->jumlah_tagihan, 0, ',', '.') . "\n";
             }
 
-            $message .= "Total Tagihan : " . number_format($tagihan->sum('jumlah_tagihan'), 0, ',', '.');
+            $message .= "Total Tagihan : Rp. " . number_format($tagihan->sum('jumlah_tagihan'), 0, ',', '.');
 
             $telegram->sendMessage([
                 'chat_id' => $chatId,
